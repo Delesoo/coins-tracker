c2a9e948e6393ee705578dae8f7a58cc30e746da
Delesoo <strongtoka@gmail.com>
2023-12-04 14:27:42 +0400

    imported use-debounced-effect

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,15 @@ import AmountInput from "./AmountInput";
 import ResultRow from "./ResultRow";
 import axios from "axios";
 import {sortBy} from 'lodash';
+import  useDebouncedEffect  from  'use-debounced-effect';
 
 type CachedResult = {
   provider: string;
   btc: string;
-}
+};
 
 function App() {
-
+  const [prevAmount,setPrevAmount] = useState('100');
   const [amount,setAmount] = useState('100');
   const [cachedResults,setCachedResults] = useState<CachedResult[]>([]);
   const [loading,setLoading] = useState(true);
@@ -22,15 +23,18 @@ function App() {
     });
   }, []);
 
-  useEffect(() => {
-    console.log('check for '+amount);
-  }, [amount]);
+  useDebouncedEffect(() => {
+    if(amount !== prevAmount) {
+      console.log('check for '+amount);
+    }
+  }, 300, [amount]);
 
   const sortedCache = sortBy(cachedResults, 'btc').reverse();
 
   return (
    <main className="max-w-2xl mx-auto px-4 py-8">
     <h1 className="uppercase text-6xl text-center font-bold bg-gradient-to-br from-pink-600 to-sky-400 bg-clip-text text-transparent from-20%">Find cheapest BTC</h1>
+
     <div className="flex justify-center mt-6">
       <AmountInput value={amount} 
                    onChange={e => setAmount(e.target.value)} />
